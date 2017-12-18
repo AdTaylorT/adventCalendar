2c39837a5435dadb3eca8ec17f7f5bb66fba62da
s7ae1k3r <adam.taylor.t@gmail.com>
2017-12-17 23:16:02 -0500

    adding day 8 to the adventure
    there are many different ways this solution can be completed
    such as dynamically coding...
    sticking with parsing each line and performing hardcoded actions

---
+++ Day8/JumpInstructions/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpInstructions
+{
+    class Program
+    {
+        public static Dictionary<string, int> registers;
+        public enum RegisterOperation { inc, dec };
+
+        /*
+         * b inc 5 if a > 1
+         * a inc 1 if b < 5
+         * c dec -10 if a >= 10
+         * c inc -20 if c == 10
+        */
+        static void Main(string[] args)
+        {
+            string input = "b inc 5 if a > 1";
+            ParseLine(input);
+            WaitToClose();
+        }
+
+        public static string ParseLine(string line)
+        {
+            StringSplitOptions opt = new StringSplitOptions();
+            RegisterOperation ro = RegisterOperation.inc;
+            
+            if (line.Contains("dec"))
+            {
+                ro = RegisterOperation.dec;
+            }
+
+            // parse the line for the actions to take
+            string[] args = {"inc", "dec", "if"};
+            string[] res = line.Split(args, opt);
+            for(int i = 0; i < res.Length; i++)
+            {
+                res[i] = res[i].Trim();
+            }
+
+            // add new "register"
+            if (! registers.ContainsKey(res[0]))
+            {
+                registers.Add(res[0], 0);
+            }
+            
+            Array.ForEach<string>(res, x => Console.WriteLine(x));
+            return "yers";
+        }
+
+        public static void WaitToClose()
+        {
+            Console.WriteLine("Closing ...");
+            Console.ReadKey();
+        }
+    }
+}
